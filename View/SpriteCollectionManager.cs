3faa7927fcbdc93a2ea3ee475f4233b977a24f50
Viktor <valambrian@users.noreply.github.com>
2021-01-28 22:22:33 -0500

    Update SpriteCollectionManager.cs
    fixing tabs/spaces mismatches

--- View/SpriteCollectionManager.cs
+++ View/SpriteCollectionManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteCollectionManager: MonoBehaviour
@@ -7,26 +7,25 @@ public class SpriteCollectionManager: MonoBehaviour
 	private SpriteCollection[] collections;
 
 	private static Dictionary<string, Texture2D> _textures = new Dictionary<string, Texture2D>();
-    private static Dictionary<string, Sprite> _sprites = new Dictionary<string, Sprite>();
+	private static Dictionary<string, Sprite> _sprites = new Dictionary<string, Sprite>();
 
-    void Awake()
+	void Awake()
 	{
 		for(int i = 0; i < collections.Length; i++)
 		{
 			this.collections[i].ParseData();
 			foreach(KeyValuePair<string, Texture2D> pair in this.collections[i].Textures)
 			{
-				
 				_textures[pair.Key] = pair.Value;
 			}
-            foreach (KeyValuePair<string, Sprite> pair in this.collections[i].Sprites)
-            {
-
-                _sprites[pair.Key] = pair.Value;
-            }
-        }
-        DontDestroyOnLoad(this);
+			foreach (KeyValuePair<string, Sprite> pair in this.collections[i].Sprites)
+			{
+				_sprites[pair.Key] = pair.Value;
+			}
+        	}
+        	DontDestroyOnLoad(this);
 	}
+
 
 	public static Texture2D GetTextureByName(string name)
 	{
@@ -40,17 +39,17 @@ public class SpriteCollectionManager: MonoBehaviour
 		}
 	}
 
-    public static Sprite GetSpriteByName(string name)
-    {
-        if (_sprites.ContainsKey(name))
-        {
-            return _sprites[name];
-        }
-        else
-        {
-            return null;
-        }
-    }
 
+	public static Sprite GetSpriteByName(string name)
+	{
+        	if (_sprites.ContainsKey(name))
+        	{
+            		return _sprites[name];
+        	}
+        	else
+        	{
+            		return null;
+        	}
+    	}
 
 }
